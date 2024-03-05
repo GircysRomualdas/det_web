afab4631b226547207da8953a7eb4c4090a8b1a1
Romualdas <r2udis@gmail.com>
2024-03-05 18:06:11 +0200

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,17 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import { Turbo } from "@hotwired/turbo-rails"
+
+
+Turbo.setConfirmMethod((message, element) => {
+    let dialog = document.getElementById("turbo-confirm")
+    dialog.querySelector("p").textContent = message
+    dialog.showModal()
+
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+            resolve(dialog.returnValue == "confirm")
+        }, {once: true})
+    })
+})
