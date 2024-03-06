98c63fe63f6710e61c09234cc523a313f63f0440
Romualdas <r2udis@gmail.com>
2024-03-06 12:23:05 +0200

    fix css

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
     has_many :details
+    before_save :update_color
     
     def self.ransackable_attributes(auth_object = nil)
         ["client", "codelist", "color", "creation_date", "dtcreation_date", "icon", "id", "count_canceled_parts", "count_changed_price_parts", "count_ordered_parts", "sum_price_wo_tax_all", "order_id", "sid", "status", "supplierlist", "vehicle_brand", "vehicle_model", "vin"]
@@ -7,5 +8,11 @@ class Order < ApplicationRecord
 
     def self.ransackable_associations(_auth_object = nil)
         []
+    end
+
+    private
+
+    def update_color
+        self.color = color.gsub('</div>', '</div><span class="progress-count">').gsub('</td>', '</span></td>')
     end
 end
